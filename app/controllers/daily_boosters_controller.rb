79e781bafaa4ec99c87fd2ef04977dd07dc48d55
Kenzo Douchet <kenzodouchet33@gmail.com>
2022-06-02 17:02:56 +0200

    add js in booster

--- app/controllers/daily_boosters_controller.rb
+++ app/controllers/daily_boosters_controller.rb
@@ -3,5 +3,11 @@ class DailyBoostersController < ApplicationController
   end
 
   def create
+    @cards = Card.order(Arel.sql('RANDOM()')).first(2)
+    @cards.each { |card| current_user.user_cards.create(card: card) }
+    respond_to do |format|
+      format.html { redirect_to user_cards_path }
+      format.js { render :create }
+    end
   end
 end
--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -2,4 +2,7 @@ class UserCard < ApplicationRecord
   belongs_to :user
   belongs_to :card
   has_many :participation_cards
+
+  validates_uniqueness_of :card_id, scope: [:user_id]
+
 end
