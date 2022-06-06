b8bccbeaebf69b0af6a8f950d016193955fdf091
Adrien SCHAFFNER <99352021+adrienschaffner@users.noreply.github.com>
2022-06-06 17:42:00 +0200

    Update rounds_controller.rb

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,6 +1,5 @@
 class RoundsController < ApplicationController
   def show
-    @user_participation = Participation.where(game: @game).where.not(user_id: current_user.id).map(&:user).first
     @round = Round.find(params[:id])
     @game = @round.game
     @participation_cards = Participation.find_by(user: current_user, game: @game)
