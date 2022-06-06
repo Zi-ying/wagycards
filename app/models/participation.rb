59e745add72c6f17aacdaa64f72a3e8d3b7d3edc
theo ounoughi <t.ounoughi@gmail.com>
2022-06-06 17:30:03 +0200

    first day on game, loading page, playing page and more

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,5 +1,12 @@
 class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :game
-  has_many :participation_cards
+  has_many :participation_card
+  after_create :create_participation_cards
+
+  def create_participation_cards
+    user.user_cards.sample(8).each do |user_card|
+      Participation_card.create(user_card: user_card, participation: self)
+    end
+  end
 end
