56160fdd7458d5a9818d2ef38ed611d2a22cc01c
Sophie Bel <sophie.belmarrocco@gmail.com>
2022-06-09 10:05:50 +0200

    do not merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :database_authenticatable, :trackable
 
   has_many :participations
   has_many :user_cards
@@ -11,4 +12,6 @@ class User < ApplicationRecord
   def online?
     updated_at > 2.minutes.ago
   end
+
+
 end
