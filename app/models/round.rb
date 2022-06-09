c85df50b69f7a6965c0e5cf2eef048f702b0e3b9
theo ounoughi <t.ounoughi@gmail.com>
2022-06-09 12:44:04 +0200

    jeu finis bébé

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,20 +2,31 @@ class Round < ApplicationRecord
   belongs_to :game
   has_many :round_cards
 
-  def winner
-    User.last
+  def winner(user)
+    winner = nil
+    game = self.game
+    opponent = game.opponent_for(user)
+    user_round_card = self.played_round_cards_for(user).last
+    opponent_round_card = self.played_round_cards_for(opponent).last
+    if opponent_round_card && user_round_card
+      result = opponent_round_card.points - user_round_card.points
+      if result.positive?
+        winner = opponent
+      else
+        winner = user
+      end
+    end
+    winner
   end
 
   def finished_for?(user)
     game = self.game
     opponent = game.opponent_for(user)
-    ##if participations_cards > 0
     user_round_card = self.available_round_cards_for(user).last
     opponent_round_card = self.available_round_cards_for(opponent).last
     finished = false
-    if opponent_round_card && user_round_card
+    if opponent_round_card && user_round_card && self.available_round_cards_for(user).count == self.available_round_cards_for(opponent).count
       result = opponent_round_card.points - user_round_card.points
-      ## delete cards from participations cards
       if result.zero?
         user_round_card.update(progress: "draw")
         opponent_round_card.update(progress: "draw")
@@ -32,20 +43,17 @@ class Round < ApplicationRecord
       end
     end
     finished
-    ##else
-      # (@end = game.progres)
-      # (@end = "done")
-      # @end.save
-      ## if opponent_round_cards.where(progress: "win").count > user.round_cards.where(progress: "win").count
-        ## redirect_to loose
-      ## else
-        ## redirect to win
-    ## end
   end
 
   def available_round_cards_for(user)
     self.round_cards
         .joins(participation_card: { user_card: :user })
         .where(user: { id: user.id }, progress: ["available", "draw"])
+  end
+
+  def played_round_cards_for(user)
+    self.round_cards
+        .joins(participation_card: { user_card: :user })
+        .where(user: { id: user.id }, progress: ["win", "loose"])
   end
 end
--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -2,8 +2,6 @@ class UserCard < ApplicationRecord
   belongs_to :user
   belongs_to :card
   has_many :participation_cards
-
-
   validates_uniqueness_of :card_id, scope: [:user_id]
 
   include PgSearch::Model
